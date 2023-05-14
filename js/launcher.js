05800463a9333fc30eabdde5f382c5bc6047b1b6
Skanda <sskanda98@gmail.com>
2023-05-15 04:10:38 +0530

    Added few Validations

--- js/launcher.js
+++ js/launcher.js
@@ -4,9 +4,16 @@ const logout = document.querySelector('.logout');
 const radianite = document.querySelector('.coins');
 const rightsidebar = document.querySelector('.right-side-bar');
 const addfriends = document.querySelector('.addfriends');
+const profilename = document.querySelector('.name');
 
 
-
+onload = () => {
+    var name = localStorage.getItem("profilename")
+    if (name != null && name != "")
+        profilename.textContent = name;
+    else
+        profilename.textContent = "Player One";
+}
 logout.addEventListener('click', function () {
     window.location.href = "../index.html";
 })
@@ -17,3 +24,5 @@ radianite.addEventListener('mouseover', function () {
 radianite.addEventListener('mouseout', function () {
     radianite.style.opacity = 1;
 })
+
+onload();
--- js/login.js
+++ js/login.js
@@ -9,54 +9,71 @@ class Login {
     validateonSubmit() {
         this.form.addEventListener('submit', (e) => {
             e.preventDefault();
+            var name = usernameinput.value
+            localStorage.setItem("profilename", name);
             var baseurl = window.location.href;
             // add any authentication logic here
-            if (usernameinput.value == "valorant" && passinput.value == "valorant123") {
+            if (passinput.value.length < 8) {
+                document.querySelector('.passerror').textContent = "Must be at least 8 characters";
+                document.querySelector('.passerror').style.color = "red";
+                document.querySelector('.passerror').style.visibility = "visible";
+                passinput.style.borderColor = "red";
+            }
+            else if (!specialChars.test(passinput.value)) {
+                document.querySelector('.passerror').textContent = "Must contain at least 1 special character";
+                document.querySelector('.passerror').style.color = "red";
+                document.querySelector('.passerror').style.visibility = "visible";
+                passinput.style.borderColor = "red";
+            }
+            else {
                 this.form.submit();
                 window.location.href = "views/loading.html";  //temp fix to encode urls
-
             }
 
-            else
-                alert("Wrong Credentials");
 
         })
     }
 
 }
 
-const eye = document.querySelector('.fa-eye-slash');
 const usernameinput = document.querySelector('.usernameinput');
+const eye = document.querySelector('.fa-eye-slash');
 const passinput = document.querySelector('.passinput');
 const fbbutton = document.querySelector('.fb');
 const submitbutton = document.querySelector('.submit');
 const loginform = document.querySelector('.loginform');
 const user = document.querySelectorAll('.user');
-
+const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
 
 
 // // eye icon functionality
-eye.addEventListener('click', function (e) {
-    e.preventDefault();
-    if (passinput.type == 'password') {
-        passinput.type = "text";
-        eye.classList.remove('fa-eye-slash');
-        eye.classList.add('fa-eye');
-    }
-    else {
-        passinput.type = "password";
-        eye.classList.remove('fa-eye');
-        eye.classList.add('fa-eye-slash');
-    }
-    if (document.hasFocus())
-        eye.style.opacity = 1;
-    else
-        eye.style.opacity = 0;
-})
+if (eye != null) {
+    eye.addEventListener('click', function (e) {
+        if (e != null) {
+            e.preventDefault();
+            if (passinput.type == 'password') {
+                passinput.type = "text";
+                eye.classList.remove('fa-eye-slash');
+                eye.classList.add('fa-eye');
+            }
+            else {
+                passinput.type = "password";
+                eye.classList.remove('fa-eye');
+                eye.classList.add('fa-eye-slash');
+            }
+            if (document.hasFocus())
+                eye.style.opacity = 1;
+            else
+                eye.style.opacity = 0;
+        }
+
+    })
+}
+
 
 // login functionality
 if (loginform) {
-    const fields = ["username", "password"];
+    const fields = [usernameinput.value, passinput.value];
     const validator = new Login(loginform, fields);
 }
 
