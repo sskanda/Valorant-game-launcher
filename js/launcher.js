f8539326b1618aebe34465e1ce78cdf5d444e876
Skanda <sskanda98@gmail.com>
2023-05-14 17:31:39 +0530

    Additional Changes

---
+++ js/launcher.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const logout = document.querySelector('.logout');
+const radianite = document.querySelector('.coins');
+const rightsidebar = document.querySelector('.right-side-bar');
+const addfriends = document.querySelector('.addfriends');
+
+
+
+logout.addEventListener('click', function () {
+    window.location.href = "../index.html";
+    console.log("logout clicked");
+})
+
+radianite.addEventListener('mouseover', function () {
+    radianite.style.opacity = 0.5;
+})
+radianite.addEventListener('mouseout', function () {
+    radianite.style.opacity = 1;
+})
